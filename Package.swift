2320e8387a9cf24bac542fa9717225ef5a404ad7
2peaches-mmichael <michael.michael@2peaches.de>
2025-07-04 23:03:45 +0200

    Enable SPM

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WYPopoverController",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "WYPopoverController",
+            targets: ["WYPopoverController"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        .target(
+            name: "WYPopoverController",
+            dependencies: [],
+            path: "Sources",
+            publicHeadersPath: "."
+        )
+    ]
+)
